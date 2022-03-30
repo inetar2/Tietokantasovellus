01b8906f091c4f3dd8681cb0fa3bb4b869c65919
Ines <ines.tarma@helsinki.fi>
2022-03-30 14:24:24 +0300

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE polls (
+    id SERIAL PRIMARY KEY,
+    topic TEXT,
+);
+CREATE TABLE choices (
+    id SERIAL PRIMARY KEY,
+    poll_id INTEGER REFERENCES polls,
+    choice TEXT
+);
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    choice_id INTEGER REFERENCES choices,
+);
